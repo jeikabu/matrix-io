1c823134b1be9b09f5c4faff3bf1c5197e1a9582
jeikabu <jeikabu@users.noreply.github.com>
2020-01-07 18:53:56 +0000

    Initial commit

---
+++ matrix-creator-hal-sys/build.rs
@@ -0,0 +1,61 @@
+fn main() {
+    link_nng();
+    bindgen_generate();
+}
+
+fn link_nng() {
+    // Run cmake to build
+    let mut config = cmake::Config::new("matrix-creator-hal");
+    let dst = config.build();
+
+    // Check output of `cargo build --verbose`, should see something like:
+    // -L native=/path/runng/target/debug/build/runng-sys-abc1234/out
+    // That contains output from cmake
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
+    );
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib64").display()
+    );
+}
+
+#[cfg(feature = "bindgen_generate")]
+fn bindgen_generate() {
+    use std::{env, path::PathBuf};
+
+    let mut builder = bindgen::Builder::default()
+        .clang_arg("-Imatrix-creator-hal/cpp/driver/")
+        .header("wrapper.hpp")
+        .derive_default(true)
+        // Generate `pub const NNG_UNIT_EVENTS` instead of `nng_unit_enum_NNG_UNIT_EVENTS`
+        .prepend_enum_name(false)
+        // Generate `pub enum ...` instead of multiple `pub const ...`
+        .default_enum_style(bindgen::EnumVariation::Rust {
+            non_exhaustive: false,
+        })
+        .use_core()
+        // Layout tests are non-portable; 64-bit tests are "wrong" size on 32-bit and always fail.
+        // Don't output tests if we're regenerating `src/bindings.rs` (shared by all platforms when bindgen not used)
+        .layout_tests(!cfg!(feature = "source-update-bindings"));
+
+    if cfg!(feature = "no_std") {
+        // no_std support
+        // https://rust-embedded.github.io/book/interoperability/c-with-rust.html#automatically-generating-the-interface
+        builder = builder.ctypes_prefix("cty")
+    }
+    let bindings = builder
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("src").join("bindings.rs"))
+        .expect("Couldn't write bindings");
+}
+
+#[cfg(not(feature = "bindgen_generate"))]
+fn bindgen_generate() {
+    // Nothing
+}
---
+++ matrix-creator-hal-sys/wrapper.hpp
@@ -0,0 +1,5 @@
+#include "microphone_array.h"
+// Enables using FIR filter with microphone array
+#include "microphone_core.h"
+// Communicates with MATRIX device
+#include "matrixio_bus.h"
